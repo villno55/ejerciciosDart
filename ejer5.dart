b8c234793e397f7a55baa95e842b202de8f15288
villno55 <adsotrabajos24@gmail.com>
2025-08-26 19:34:06 -0500

    Algunas variables

--- ejer5.dart
+++ ejer5.dart
@@ -1,22 +1,22 @@
 import 'dart:io';
 
 void main() {
-  print("la calculadora de descuentos");
+  print("la calculadora de descuentos");//le da contexto al usuario
 
-  stdout.write("Ingrese monto de compra: ");
+  stdout.write("Ingresa el valor de compra: ");
   double monto = double.parse(stdin.readLineSync()!);
 
-  double descuento = 0;
-  if (monto >= 201) descuento = 0.15;
-  else if (monto >= 101) descuento = 0.10;
-  else if (monto >= 51) descuento = 0.05;
+  double descu = 0;
+  if (monto >= 201) descu = 0.15;
+  else if (monto >= 101) descu = 0.10;
+  else if (monto >= 51) descu = 0.05;
 
-  double ahorro = monto * descuento;
+  double ahorro = monto * descu;
   double subtotal = monto - ahorro;
   double iva = subtotal * 0.19;
   double total = subtotal + iva;
 
-  print("Descuento aplicado: ${descuento * 100}%");
+  print("el descuento aplicado es: ${descu * 100}%");
   print("Ahorro: \$${ahorro.toStringAsFixed(2)}");
   print("Total con IVA: \$${total.toStringAsFixed(2)}");
 }
